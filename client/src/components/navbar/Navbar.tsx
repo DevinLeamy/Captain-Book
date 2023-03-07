a2284ab7170ebbdda153f4b6ec9951d9658aa25a
Devin Leamy <devinleamy@gmail.com>
2023-03-06 22:52:31 -0500

    Navbar display border around the currently selected tab

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -2,28 +2,36 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 import { useAuth } from "../../hooks/useAuth"
+import { useLocation } from "react-router-dom"
 
 import "./Navbar.css"
 
 const Navbar = () => {
     const { authenticated, onLogin, onLogout } = useAuth()
+    const location = useLocation().pathname
     return (
         <div className="navbar-container">
             <div className="navbar-left">📚 Nouvelle</div>
             <div className="navbar-right">
-                <Link className="navbar-link" to={"./search"}>
+                <Link
+                    className={`navbar-link ${location === "/search" ? "selected-link" : ""}`}
+                    to={"./search"}
+                >
                     Search
                 </Link>
-                <Link className="navbar-link" to={"./library"}>
+                <Link
+                    className={`navbar-link ${location === "/library" ? "selected-link" : ""}`}
+                    to={"./library"}
+                >
                     Library
                 </Link>
                 {!authenticated && (
-                    <div className="navbar-link login-button" onClick={onLogin}>
+                    <div className="navbar-link" onClick={onLogin}>
                         Login
                     </div>
                 )}
                 {authenticated && (
-                    <div className="navbar-link login-button" onClick={onLogout}>
+                    <div className="navbar-link" onClick={onLogout}>
                         Logout
                     </div>
                 )}
